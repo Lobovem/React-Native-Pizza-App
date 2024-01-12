df28e240dc10a09e26b17a403220f0cf48e627a0
Lobov.EM <lobov.em@gmail.com>
2024-01-12 17:32:02 +0200

    refactor(SettingScreen): add TS to SettingScreen

--- src/screens/home/SettingsScreen.tsx
+++ src/screens/home/SettingsScreen.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export const SettingsScreen = () => {
+export const SettingsScreen: FC = () => {
   return (
     <SafeAreaView style={styles.wrap}>
       <Text>Settings Screen</Text>
