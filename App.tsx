c37506905df103632c670571bb8ea6a9f960489f
Lobov.EM <lobov.em@gmail.com>
2024-01-19 12:38:33 +0200

    feat(App background): add to app useStateApp, but dont work absolute

--- App.tsx
+++ App.tsx
@@ -1,8 +1,30 @@
 import { FC } from 'react';
 import { Navigation } from './src/navigation/Navigation';
+import { useAppState } from './src/hooks/useAppState';
+import { Image, StyleSheet } from 'react-native';
+
+import hiddenBackground from './src/screens/home/img/hidden-background.jpeg';
 
 const App: FC = () => {
-  return <Navigation />;
+  const app = useAppState();
+  console.log(app);
+
+  return (
+    <>
+      {app === 'inactive' && (
+        <Image source={hiddenBackground} style={styles.wrap}></Image>
+      )}
+      <Navigation />
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  wrap: {
+    ...StyleSheet.absoluteFillObject,
+    width: '100%', // или другие значения по необходимости
+    height: '100%',
+  },
+});
 
 export default App;
--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import { HomeTabs } from './HomeTabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { useAppState } from '../hooks/useAppState';
+import { Image, StyleSheet, View } from 'react-native';
+import hiddenBackground from '../screens/home/img/hidden-background.jpeg';
 
 export const Navigation = () => {
+  const app = useAppState();
+  console.log(app);
   return (
     <NavigationContainer>
+      {/* <View style={{ position: 'relative' }}>
+        {app === 'inactive' && (
+          <Image source={hiddenBackground} style={styles.wrap}></Image>
+        )}
+      </View> */}
       <HomeTabs />
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  wrap: {
+    ...StyleSheet.absoluteFillObject,
+    // backgroundColor: 'red',
+    // position: 'relative',
+    // width: 500,
+    // height: 1000,
+  },
+});
