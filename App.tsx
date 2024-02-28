ed35f0398db71f22bc5c47048b86075ebe169125
Lobov.EM <lobov.em@gmail.com>
2024-02-28 17:14:51 +0200

    feat(Authorization): install packages

--- App.tsx
+++ App.tsx
@@ -1,27 +1,26 @@
 import { FC } from 'react';
 import { useAppState } from './src/hooks/useAppState';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, Text } from 'react-native';
 
 import hiddenBackground from './src/Screens/HomeScreen/img/hidden-background.png';
 import { Navigation } from './src/navigation/Navigation';
+import { ClerkProvider } from '@clerk/clerk-expo';
 
 const App: FC = () => {
+  const CLERK_PUBLISHABLE_KEY =
+    'pk_test_dG91Y2hpbmctYmVkYnVnLTQ0LmNsZXJrLmFjY291bnRzLmRldiQ';
   const app = useAppState();
 
   return (
+    // <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+    //   <Text>TEST</Text>
+    // </ClerkProvider>
+
     <>
-      {app === 'inactive' ? (
-        <ImageBackground source={hiddenBackground} style={styles.wrap} />
-      ) : (
-        <Navigation />
-      )}
+      <ImageBackground source={hiddenBackground} style={styles.wrap} />
+
+      {app !== 'inactive' && <Navigation />}
     </>
-
-    // <>
-    //   <ImageBackground source={hiddenBackground} style={styles.wrap} />
-
-    //   {app !== 'inactive' && <Navigation />}
-    // </>
   );
 };
 
