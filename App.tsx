65bab3cbcc021aa22f6e83d512a86f04b9e65adf
Lobov.EM <lobov.em@gmail.com>
2024-02-28 18:03:03 +0200

    feat(Authorization): do base a feature

--- App.tsx
+++ App.tsx
@@ -1,30 +1,40 @@
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
 import { useAppState } from './src/hooks/useAppState';
-import { ImageBackground, StyleSheet, Text } from 'react-native';
+import { ImageBackground, SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
 
 import hiddenBackground from './src/Screens/HomeScreen/img/hidden-background.png';
 import { Navigation } from './src/navigation/Navigation';
-import { ClerkProvider } from '@clerk/clerk-expo';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import Login from './src/Screens/LoginScreen/Login';
 
 const App: FC = () => {
-  const CLERK_PUBLISHABLE_KEY =
-    'pk_test_dG91Y2hpbmctYmVkYnVnLTQ0LmNsZXJrLmFjY291bnRzLmRldiQ';
   const app = useAppState();
 
   return (
-    // <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
-    //   <Text>TEST</Text>
-    // </ClerkProvider>
-
-    <>
-      <ImageBackground source={hiddenBackground} style={styles.wrap} />
-
-      {app !== 'inactive' && <Navigation />}
-    </>
+    <SafeAreaView style={styles.container}>
+      <ClerkProvider publishableKey="pk_test_aG9wZWZ1bC1tb2xsdXNrLTI1LmNsZXJrLmFjY291bnRzLmRldiQ">
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <Login />
+        </SignedOut>
+      </ClerkProvider>
+    </SafeAreaView>
   );
+  //   <>
+  //     <ImageBackground source={hiddenBackground} style={styles.wrap} />
+
+  //     {app !== 'inactive' && <Navigation />}
+  //   </>
+  // );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    flex: 1,
+  },
   wrap: {
     // ...StyleSheet.absoluteFillObject,
     position: 'absolute',
--- src/Screens/LoginScreen/Login.tsx
+++ src/Screens/LoginScreen/Login.tsx
@@ -1,10 +1,56 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useCallback } from 'react';
+import ColorsVariable from '../../components/ColorsVariable';
+import { useOAuth } from '@clerk/clerk-expo';
+import * as WebBrowser from 'expo-web-browser';
+import { useWarmUpBrowser } from '../../hooks/useWarmUpBrowser';
 
-export default function Login() {
+WebBrowser.maybeCompleteAuthSession();
+
+const Login = () => {
+  useWarmUpBrowser();
+  const { startOAuthFlow } = useOAuth({ strategy: 'oauth_google' });
+
+  const onPress = useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } = await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error('OAuth error', err);
+    }
+  }, []);
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.wrapComponent}>
+      <TouchableOpacity style={styles.btn} onPress={onPress}>
+        <Text style={styles.btnTitle}>Let's Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  wrapComponent: {},
+
+  btn: {
+    width: '100%',
+    height: 40,
+    backgroundColor: ColorsVariable.orange,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  btnTitle: {
+    color: ColorsVariable.white,
+    fontSize: 20,
+    fontWeight: '500',
+  },
+});
